12d29a96ae28618db1d1ef951fcab1b3e645631f
Kat Tornwall <ktornwall@gmail.com>
2018-07-19 14:56:57 -0400

    Step 3 - Part 3 - Moving the HTML into our component

--- src/App.js
+++ src/App.js
@@ -17,28 +17,6 @@ class App extends Component {
           <label htmlFor="toggle-all">Mark all as complete</label>
 
           <Todos />
-          <ul className="todo-list">
-            {/* These are here just to show the structure of the list items */}
-            {/* List items should get the class `editing` when editing and `completed` when marked as completed */}
-            <li className="completed">
-              <div className="view">
-                <input className="toggle" type="checkbox" checked />
-                <label>Taste JavaScript</label>
-                <button className="destroy"></button>
-              </div>
-
-              <input className="edit" value="Create a TodoMVC template" />
-            </li>
-
-            <li>
-              <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>Buy a unicorn</label>
-                <button className="destroy"></button>
-              </div>
-              <input className="edit" value="Rule the web" />
-            </li>
-          </ul>
         </section>
 
         {/* This footer should hidden by default and shown when there are todos */}
