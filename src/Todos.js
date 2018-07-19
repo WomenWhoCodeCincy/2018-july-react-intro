23abc8beab5a9612b393b701c0cc0baa21c77f56
Kat Tornwall <ktornwall@gmail.com>
2018-07-19 14:59:32 -0400

    Step 3 - Part 4 - Rendering a list

--- src/Todos.js
+++ src/Todos.js
@@ -2,28 +2,26 @@ import React, { Component } from 'react';
 
 class Todos extends Component {
   render() {
+    const todos = [
+      {},
+      {},
+      {},
+    ];
+
     return (
       <ul className="todo-list">
-        {/* These are here just to show the structure of the list items */}
-        {/* List items should get the class `editing` when editing and `completed` when marked as completed */}
-        <li className="completed">
-          <div className="view">
-            <input className="toggle" type="checkbox" checked />
-            <label>Taste JavaScript</label>
-            <button className="destroy"></button>
-          </div>
-
-          <input className="edit" value="Create a TodoMVC template" />
-        </li>
-
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Buy a unicorn</label>
-            <button className="destroy"></button>
-          </div>
-          <input className="edit" value="Rule the web" />
-        </li>
+        {
+          todos.map((todo) => (
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>Buy a unicorn</label>
+                <button className="destroy"></button>
+              </div>
+              <input className="edit" value="Rule the web" />
+            </li>
+          ))
+        }
       </ul>
     );
   }
